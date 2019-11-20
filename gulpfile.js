f4b646aceef0a6461e4f8105948e2fc983795ed9
Matt Sigmon <matt@raven.re>
2019-11-20 15:31:06 -0500

    more updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,102 @@
 // Load all the modules from package.json
 var gulp = require('gulp'),
-  plugins = require('gulp-load-plugins')();
+	plugins = require('gulp-load-plugins')();
 var events = require('events');
 var emitter = new events.EventEmitter();
 var path = require('path');
 var gutil = require('gulp-util');
 var currentTask = '';
 var plumber = require('gulp-plumber'),
-  watch = require('gulp-watch'),
-  //livereload = require('gulp-livereload'),
-  minifycss = require('gulp-clean-css'),
-  jshint = require('gulp-jshint'),
-  stylish = require('jshint-stylish'),
-  uglify = require('gulp-uglify-es').default,
-  rename = require('gulp-rename'),
-  notify = require('gulp-notify'),
-  include = require('gulp-include'),
-  sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  imagemin = require('gulp-imagemin'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  gutil = require('gulp-util');
+	watch = require('gulp-watch'),
+	//livereload = require('gulp-livereload'),
+	minifycss = require('gulp-clean-css'),
+	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish'),
+	uglify = require('gulp-uglify-es').default,
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	include = require('gulp-include'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	imagemin = require('gulp-imagemin'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	gutil = require('gulp-util');
 
 // Set assets paths.
 const paths = {
-  'css': ['./*.css', '!*.min.css'],
-  'icons': 'assets/images/svg-icons/*.svg',
-  'images': ['assets/images/*', '!assets/images/*.svg'],
-  'php': ['./*.php', './**/*.php'],
-  'sass': 'assets/scss/**/*.scss',
-  'concat_scripts': 'assets/js/concat/*.js',
-  'scripts': ['assets/js/*.js', '!assets/scripts/*.min.js'],
-  'sprites': 'assets/images/sprites/*.png'
+	css: ['./*.css', '!*.min.css'],
+	icons: 'assets/images/svg-icons/*.svg',
+	images: ['assets/images/*', '!assets/images/*.svg'],
+	php: ['./*.php', './**/*.php'],
+	sass: 'assets/scss/**/*.scss',
+	concat_scripts: 'assets/js/concat/*.js',
+	scripts: ['assets/js/*.js', '!assets/scripts/*.min.js'],
+	sprites: 'assets/images/sprites/*.png'
 };
 
-
 // Default error handler
-var reportError = function (error) {
+var reportError = function(error) {
+	var lineNumber = error.lineNumber ? 'LINE ' + error.lineNumber + ' -- ' : '';
+	var pluginName = error.plugin
+		? ': [' + error.plugin + ']'
+		: '[' + currentTask + ']';
 
-  var lineNumber = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '';
-  var pluginName = (error.plugin) ? ': [' + error.plugin + ']' : '[' + currentTask + ']';
+	plugins
+		.notify({
+			title: 'You F***ed Up! ' + pluginName,
+			message: lineNumber + 'See console.'
+		})
+		.write(error);
 
-  plugins.notify({
-    title: 'You F***ed Up! ' + pluginName,
-    message: lineNumber + 'See console.'
-  }).write(error);
+	gutil.beep();
 
-  gutil.beep();
+	var report = '';
+	var chalk = gutil.colors.white.bgRed;
 
-  var report = '';
-  var chalk = gutil.colors.white.bgRed;
+	report += chalk('TASK:') + pluginName + '\n';
+	report += chalk('ERROR:') + ' ' + error.message + '\n';
+	if (error.lineNumber) {
+		report += chalk('LINE:') + ' ' + error.lineNumber + '\n';
+	}
+	if (error.fileName) {
+		report += chalk('FILE:') + ' ' + error.fileName + '\n';
+	}
 
-  report += chalk('TASK:') + pluginName + '\n';
-  report += chalk('ERROR:') + ' ' + error.message + '\n';
-  if (error.lineNumber) {
-    report += chalk('LINE:') + ' ' + error.lineNumber + '\n';
-  }
-  if (error.fileName) {
-    report += chalk('FILE:') + ' ' + error.fileName + '\n';
-  }
+	console.error(report);
 
-  console.error(report);
-
-  this.emit('end');
-}
-
+	this.emit('end');
+};
 
 // Jshint outputs any kind of javascript problems you might have
 // Only checks javascript files inside /src directory
 
-
-
-gulp.task('jshint', function () {
-  return gulp.src('./js/src/*.js')
-  //.pipe( jshint( '.jshintrc' ) ) uncomment for JS Debugging (Very Strict)
-    .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter('fail'));
-})
-
+gulp.task('jshint', function() {
+	return (
+		gulp
+			.src('./js/src/*.js')
+			//.pipe( jshint( '.jshintrc' ) ) uncomment for JS Debugging (Very Strict)
+			.pipe(jshint.reporter(stylish))
+			.pipe(jshint.reporter('fail'))
+	);
+});
 
 // Concatenates all files that it finds in the manifest
 // and creates two versions: normal and minified.
 // It's dependent on the jshint task to succeed.
-gulp.task('scripts', function () {
-  return gulp.src('./js/manifest.js')
-    .pipe(include())
-    .pipe(rename({ basename: 'scripts' }))
-    .pipe(gulp.dest('./js/dist'))
+gulp.task('scripts', function() {
+	return (
+		gulp
+			.src('./js/manifest.js')
+			.pipe(include())
+			.pipe(rename({basename: 'scripts'}))
+			.pipe(gulp.dest('./js/dist'))
 
-    // .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./js/dist'))
-    //.pipe(livereload());
+			// .pipe(uglify())
+			.pipe(rename({suffix: '.min'}))
+			.pipe(gulp.dest('./js/dist'))
+	);
+	//.pipe(livereload());
 });
 
 //----------------------------------------------
@@ -101,55 +105,57 @@ gulp.task('scripts', function () {
 // Sourcemap Creation
 //----------------------------------------------
 
-gulp.task('scss', function () {
-  return gulp.src('./scss/custom.scss')
-    .pipe(plumber({ errorHandler: reportError }))
+gulp.task('scss', function() {
+	return (
+		gulp
+			.src('./scss/custom.scss')
+			.pipe(plumber({errorHandler: reportError}))
 
-    // Init Sourcemaps
-    .pipe(sourcemaps.init())
+			// Init Sourcemaps
+			.pipe(sourcemaps.init())
 
-    // Compile SASS
-    .pipe(sass())
+			// Compile SASS
+			.pipe(sass())
 
-    // Send to CSS file
-    .pipe(gulp.dest('./css'))
+			// Send to CSS file
+			.pipe(gulp.dest('./css'))
 
-    // Load Sourcsmaps from SASS
-    .pipe(sourcemaps.init({ loadMaps: true }))
+			// Load Sourcsmaps from SASS
+			.pipe(sourcemaps.init({loadMaps: true}))
 
-    // Parse with PostCSS plugins.
-    .pipe(postcss([
-      autoprefixer({
-        'browsers': ['last 2 version']
-      })
-    ]))
+			// Parse with PostCSS plugins.
+			.pipe(
+				postcss([
+					autoprefixer({
+						overrideBrowserslist: ['last 2 version']
+					})
+				])
+			)
 
-    // Minify CSS
-    //.pipe(minifycss())
+			// Minify CSS
+			//.pipe(minifycss())
 
-    // Rename file
-    .pipe(rename({ suffix: '.min' }))
+			// Rename file
+			.pipe(rename({suffix: '.min'}))
 
-    // Generate our new, super helpful sourcemaps
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'))
-    
+			// Generate our new, super helpful sourcemaps
+			.pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest('./css'))
+	);
 
-  // Exit this task
+	// Exit this task
 });
-
 
 // Start the livereload server and watch files for change
-gulp.task('watch', function () {
-  
-
-  // don't listen to whole js folder, it'll create an infinite loop
-  gulp.watch( ['./js/**/*.js', '!./assets/js/dist/*.js'], gulp.parallel( ['scripts'] ) )
-  gulp.watch('./scss/**/*.scss', gulp.parallel( ['scss'] ) );
-  gulp.watch('./**/*.php').on('change', function (file) {
-  });
+gulp.task('watch', function() {
+	// don't listen to whole js folder, it'll create an infinite loop
+	gulp.watch(
+		['./js/**/*.js', '!./assets/js/dist/*.js'],
+		gulp.parallel(['scripts'])
+	);
+	gulp.watch('./scss/**/*.scss', gulp.parallel(['scss']));
+	gulp.watch('./**/*.php').on('change', function(file) {});
 });
-
 
 /**
  * Optimize images.
@@ -157,25 +163,31 @@ gulp.task('watch', function () {
  * https://www.npmjs.com/package/gulp-imagemin
  */
 gulp.task('imagemin', () =>
-  gulp.src(paths.images)
-    .pipe(plumber({ 'errorHandler': reportError }))
-    .pipe(imagemin([imagemin.gifsicle({ interlaced: true }),
-      imagemin.jpegtran({ progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ],
-      })
-    ], {
-      verbose: true // Let me know specifics!
-    }))
-    .pipe(gulp.dest('./images'))
+	gulp
+		.src(paths.images)
+		.pipe(plumber({errorHandler: reportError}))
+		.pipe(
+			imagemin(
+				[
+					imagemin.gifsicle({interlaced: true}),
+					imagemin.jpegtran({progressive: true}),
+					imagemin.optipng({optimizationLevel: 5}),
+					imagemin.svgo({
+						plugins: [{removeViewBox: true}, {cleanupIDs: false}]
+					})
+				],
+				{
+					verbose: true // Let me know specifics!
+				}
+			)
+		)
+		.pipe(gulp.dest('./images'))
 );
 
-
 // TODO: Find a different way to run watch on default
-gulp.task( 'default', gulp.series( ['watch'], function() {
-  // Does nothing in this task, just triggers the dependent 'watch'
-} ) );
+gulp.task(
+	'default',
+	gulp.series(['watch'], function() {
+		// Does nothing in this task, just triggers the dependent 'watch'
+	})
+);
--- single.php
+++ single.php
@@ -1,46 +1,47 @@
 <?php get_header(); ?>
 <?php get_template_part('partials/template-part', 'head'); ?>
-<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+<?php $thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
 <article id="category-page">
-    <section id="hero" class="inner-page <?php the_field('header_class') ;?>" style="background: url('<?php echo $thumb['0'];?>') 50% 50%; background-size: cover;">
+    <section id="hero" class="inner-page <?php the_field('header_class'); ?>" style="background: url('<?php echo $thumb['0']; ?>') 50% 50%; background-size: cover;">
         <header>
             <h1><?php the_title(); ?></h1>
         </header>
     </section>
     <section class="container single-blog">
         <div class="row">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <header>
-                <h2><?php the_field('sub_heading') ;?></h2>
-                <h3 class="postmeta">Posted on <span class="date"><?php the_date(); ?></span> by <span class="author"><?php the_author(); ?> </a> in <span class="category"><?php the_category(', '); ?></span></h3>
-            </header>
-            <?php
-              /* Get page slug to provide unique class for each post*/
-              $slug = get_post_field( 'post_name', get_post() );
-            ?>
-            <div class="article <?php echo $slug; ?>">
-                <div>
-                    <?php the_content(); ?>
-                </div>
-            </div>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <header>
+                        <h2><?php the_field('sub_heading'); ?></h2>
+                        <h3 class="postmeta">Posted on <span class="date"><?php the_date(); ?></span> by <span class="author"><?php the_author(); ?> </a> in <span class="category"><?php the_category(', '); ?></span></h3>
+                    </header>
+                    <?php
+                            /* Get page slug to provide unique class for each post*/
+                            $slug = get_post_field('post_name', get_post());
+                            ?>
+                    <article class="blog-article <?php echo $slug; ?>">
+                        <div>
+                            <?php the_content(); ?>
+                        </div>
+                    </article>
         </div>
         <!--BEGIN .author-bio-->
         <div class="row author-bio">
             <div class="col-md-3">
-                <?php echo get_avatar( get_the_author_meta('user_email'), $size = 'full'); ?>
+                <?php echo get_avatar(get_the_author_meta('user_email'), $size = 'full'); ?>
             </div>
             <div class="col-md-9">
                 <div class="author-info">
-                   <h3 class="author-title">Written by <?php the_author_link(); ?></h3>
-                   <p class="author-description"><?php the_author_meta('description'); ?></p>
-               </div>
-           </div>
-           <!--END .author-bio-->
+                    <h3 class="author-title">Written by <?php the_author_link(); ?></h3>
+                    <p class="author-description"><?php the_author_meta('description'); ?></p>
+                </div>
+            </div>
+            <!--END .author-bio-->
         </div>
-        <?php endwhile; ?>
-        <?php else: ?>
-            <?php get_404_template(); ?>
-        <?php endif; ?>
-    </section><!--.container-->
+    <?php endwhile; ?>
+<?php else : ?>
+    <?php get_404_template(); ?>
+<?php endif; ?>
+    </section>
+    <!--.container-->
 </article>
 <?php get_footer(); ?>
