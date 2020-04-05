92a8db371f742900dc5e7bfe5cdcf3fbf408e1af
Matt Sigmon <matt@raven.re>
2020-04-05 11:59:03 -0400

    Temp COVID header and npm update

--- front-page.php
+++ front-page.php
@@ -1,36 +1,85 @@
 <?php get_header(); ?>
 <?php get_template_part('partials/template-part', 'head'); ?>
 <article id="homepage">
-	<section id="hero" class="home">
-		<header>
-			<h1>Family Dentist in Garner, NC</h1>
-			<h2>Welcome to White Oak Family Dentistry</h2>
-		</header>
-	</section>
-	<section id="home-content">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<h3>Full Service General Dentistry</h3>
-					<p>White Oak Family Dentistry in Garner, NC sets the highest standard for quality dental care. You and your entire family are invited to our new facility that provides comprehensive treatment for your oral health. From aesthetic enhancements to dental reconstruction, we are a full-service dental office that uses advanced technology and a gentle touch to perform every dental procedure. If fear has been your obstacle to receiving care, we offer anti-anxiety options to help facilitate your dental experience. Whether you are looking for a new dental home for routine care or you have an emergency, allow the team at White Oak Family Dentistry to give you a comfortable and memorable visit.</p>
-				</div>
-				<div class="col-md-6">
-					<h3>Dental Care &amp; Cosmetic Enhancements</h3>
-          			<p>With routine check-ups, our patients can maintain healthy teeth and gums. Poor dental health can have a negative impact on overall physical health. Untreated tooth infections have shown to be connected to heart attacks, strokes, and diabetes complications. White Oak Family Dentistry provides top quality <a href="<?php echo esc_url( home_url( '/' ) ); ?>services/">dental services</a> to protect both your smile and your physical health. Our friendly team is here to provide a variety of cosmetic enhancements, including <a href="<?php echo esc_url( home_url( '/' ) ); ?>veneers/">Veneers</a>, <a href="<?php echo esc_url( home_url( '/' ) ); ?>whitening/">Teeth Whitening</a>, <a href="<?php echo esc_url( home_url( '/' ) ); ?>invisalign/">Invisalign</a>, and much more! White Oak Family Dentistry is here to serve communities throughout the Raleigh area - Garner, Clayton, Willow Springs and Apex. </p>
-				</div>
-			</div><!--.row-->
-			<div class="row">
-				<div class="col-md-12">
-					<div class="home-schedule">
-						<h3>Schedule Your Appointment Today - White Oak Family Dentistry</h3>
-						<p>As with any preventative dental care, timing is everything! Let Dr. Sigmon get you started on a plan that promises healthy and beautiful smiles. White Oak Family Dentistry is offering a lifetime of trustworthy oral and cosmetic care. Our top priority is making sure you know the true condition of your teeth and gums. Trust us with your check ups, cleanings, teeth whitening, plaque removal, cavity fillings, crowns and more. If an accident has caused your smile to disappear, we’ll bring it back with quality veneers, <a href="<?php echo esc_url( home_url( '/' ) ); ?>bridges/">bridges</a>, and <a href="<?php echo esc_url( home_url( '/' ) ); ?>dentures/">dentures</a>. Give us a call today to discuss all the options that are available to you at White Oak Family Dentistry.</p>
-						<div class="centered">
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>appointments/">Schedule Today</a>
-						</div>
-					</div>
-				</div><!--.col-md-12-->
-			</div><!--.row-->
-		</div><!--.container-->
-	</section><!--.home-content-->
+
+    <section id="hero" class="home">
+        <!--
+        <header>
+            <h1>Family Dentist in Garner, NC</h1>
+            <h2>Welcome to White Oak Family Dentistry</h2>
+        </header>
+	-->
+        <header>
+            <h1>COVID-19</h1>
+            <h2>We are open for emergency treatment</h2>
+            <p>The Coronavirus (COVID-19) pandemic has become a greater impact on
+                our society in the last few weeks. We have been monitoring all communications from the North Carolina
+                Dental Board,
+                American Dental Association (ADA), Center for Disease Control (CDC), and North Carolina Department of
+                Health and Human Services (NCDHHS). The current recommendation published by the NC Dental Board is to
+                postpone all elective or non-emergent dental treatment.</p>
+            <p>Therefore, White Oak Family Dentistry has decided to postpone all routine and elective treatment until
+                April 30th. As soon as it is safe, we will resume normal hours of operation and work diligently to get
+                all of our patients seen as soon as possible.</p>
+
+        </header>
+
+    </section>
+    <section id="home-content">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Full Service General Dentistry</h3>
+                    <p>White Oak Family Dentistry in Garner, NC sets the highest standard for quality dental care. You
+                        and your entire family are invited to our new facility that provides comprehensive treatment for
+                        your oral health. From aesthetic enhancements to dental reconstruction, we are a full-service
+                        dental office that uses advanced technology and a gentle touch to perform every dental
+                        procedure. If fear has been your obstacle to receiving care, we offer anti-anxiety options to
+                        help facilitate your dental experience. Whether you are looking for a new dental home for
+                        routine care or you have an emergency, allow the team at White Oak Family Dentistry to give you
+                        a comfortable and memorable visit.</p>
+                </div>
+                <div class="col-md-6">
+                    <h3>Dental Care &amp; Cosmetic Enhancements</h3>
+                    <p>With routine check-ups, our patients can maintain healthy teeth and gums. Poor dental health can
+                        have a negative impact on overall physical health. Untreated tooth infections have shown to be
+                        connected to heart attacks, strokes, and diabetes complications. White Oak Family Dentistry
+                        provides top quality <a href="<?php echo esc_url(home_url('/')); ?>services/">dental
+                            services</a> to protect both your smile and your physical health. Our friendly team is here
+                        to provide a variety of cosmetic enhancements, including <a
+                            href="<?php echo esc_url(home_url('/')); ?>veneers/">Veneers</a>, <a
+                            href="<?php echo esc_url(home_url('/')); ?>whitening/">Teeth Whitening</a>, <a
+                            href="<?php echo esc_url(home_url('/')); ?>invisalign/">Invisalign</a>, and much more! White
+                        Oak Family Dentistry is here to serve communities throughout the Raleigh area - Garner, Clayton,
+                        Willow Springs and Apex. </p>
+                </div>
+            </div>
+            <!--.row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="home-schedule">
+                        <h3>Schedule Your Appointment Today - White Oak Family Dentistry</h3>
+                        <p>As with any preventative dental care, timing is everything! Let Dr. Sigmon get you started on
+                            a plan that promises healthy and beautiful smiles. White Oak Family Dentistry is offering a
+                            lifetime of trustworthy oral and cosmetic care. Our top priority is making sure you know the
+                            true condition of your teeth and gums. Trust us with your check ups, cleanings, teeth
+                            whitening, plaque removal, cavity fillings, crowns and more. If an accident has caused your
+                            smile to disappear, we’ll bring it back with quality veneers, <a
+                                href="<?php echo esc_url(home_url('/')); ?>bridges/">bridges</a>, and <a
+                                href="<?php echo esc_url(home_url('/')); ?>dentures/">dentures</a>. Give us a call today
+                            to discuss all the options that are available to you at White Oak Family Dentistry.</p>
+                        <div class="centered">
+                            <a href="<?php echo esc_url(home_url('/')); ?>appointments/">Schedule Today</a>
+                        </div>
+                    </div>
+                </div>
+                <!--.col-md-12-->
+            </div>
+            <!--.row-->
+        </div>
+        <!--.container-->
+    </section>
+    <!--.home-content-->
 </article>
 <?php get_footer(); ?>
